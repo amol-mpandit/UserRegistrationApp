6b21ef6a9d8844dd72344e963d7c48fcf5e8f92d
Amol Pandit <amol.mpandit@gmail.com>
2018-12-26 02:06:45 +0530

    added other files

---
+++ UserRegistrationApp/Controllers/UsersController.cs
@@ -0,0 +1,115 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using Core;
+using Persistance;
+
+namespace UserRegistrationApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UserRepository _userRepository;
+
+        public UsersController(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var users = _userRepository.GetAllUser();
+            return View(users);
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = _userRepository.Get(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,userName,FirstName,MiddleName,LastName,Email,Phone,Address,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _userRepository.Add(user);
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = _userRepository.Get(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,userName,FirstName,MiddleName,LastName,Email,Phone,Address,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _userRepository.Update(user);
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = _userRepository.Get(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _userRepository.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
